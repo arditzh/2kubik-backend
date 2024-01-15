f1abd4e471c8ceab634a5938d6d6f8ee1ef65ee5
Ardit Zherka <arditzherka@gmail.com>
2024-01-15 22:54:48 +0100

    change

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return ' Here we are >>>>2ooo Kubik <<<<<!';
+    return ' Here we Aare >>>>2ooo Kubik <<<<<!';
   }
 }
